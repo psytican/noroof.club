e24e3e85ca9234d134738d37fc00da0c7fe76281
Evgeny Panov <evgeny.panov@xgroup.cy>
2024-10-18 12:53:31 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,8 +2,30 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/js/service-worker.js').then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
+
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            console.log('New content is available; please refresh.');
+                            if (confirm('New version available. Refresh now?')) {
+                                window.location.reload();
+                            }
+                        } else {
+                            console.log('Content is cached for offline use.');
+                        }
+                    }
+                };
+            };
         }).catch((error) => {
             console.error('Service Worker registration failed:', error);
         });
+    });
+
+    navigator.serviceWorker.getRegistration().then(function(registration) {
+        if (registration) {
+            registration.update();
+        }
     });
 }
--- js/service-worker.js
+++ js/service-worker.js
@@ -1,14 +1,60 @@
+const CACHE_NAME = 'noroof-club--cache-version--2';
+
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/radio-ambient.html',
+    '/radio-drumnbass.html',
+    '/radio-gettotech.html',
+    '/radio-psydub.html',
+    '/radio-psytrance.html',
+    '/css/0.css',
+    '/css/480.css',
+    '/css/768.css',
+    '/css/960.css',
+    '/css/1220.css',
+    '/css/colors.css',
+    '/css/default.css',
+    '/css/footer.css',
+    '/css/header.css',
+    '/css/index.css',
+    '/css/main-clean.css',
+    '/css/main-map.css',
+    '/css/main-player.css',
+    '/css/main-radio.css',
+    '/js/main.js',
+    '/js/radio.js',
+    '/js/service-worker.js',
+    '/images/button-play.svg',
+    '/images/button-stop.svg',
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png',
+    '/images/loading.gif',
+    '/images/logo.svg',
+];
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-cache').then((cache) => {
-            return cache.addAll([
-                '/images/icon-192x192.png',
-                '/images/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
